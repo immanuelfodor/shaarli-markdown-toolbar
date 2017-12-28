f421d972024d656ace94c69966155a687055fdfd
immanuelfodor <immanuelfactor+github@gmail.com>
2017-12-28 14:27:55 +0100

    configuring the markdown toolbar init parameters

--- markdown_toolbar/includes/markdown_toolbar.js
+++ markdown_toolbar/includes/markdown_toolbar.js
@@ -1,5 +1,18 @@
 (function ($) {
     $(document).ready(function() {
-        $("#lf_description").markdown({iconlibrary:"fa"});
+        $("#lf_description").markdown({
+            autofocus: true,
+            iconlibrary: "fa", 
+            resize: "vertical",
+            disabledButtons: ["cmdPreview"],
+            language: "en",
+            footer: '<small id="desc-counter" class="text-muted">Chars: 0</small>',
+            onChange: function(e){
+                var content = e.parseContent(),
+                    content_length = (content.match(/\n/g)||[]).length + content.length;
+                
+                $('#desc-counter').html('Chars: ' + content_length);
+            }
+        });
     });
 })(jQuery.noConflict(true));
