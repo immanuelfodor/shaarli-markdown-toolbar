dad23140beab49e2f0f8cde7511f17dc0677cf2a
immanuelfodor <immanuelfactor+github@gmail.com>
2017-12-28 14:39:29 +0100

    displaying content length when plugin loads

--- markdown_toolbar/includes/markdown_toolbar.js
+++ markdown_toolbar/includes/markdown_toolbar.js
@@ -1,4 +1,16 @@
 (function ($) {
+
+    var getContentLength = function(e) {
+            var content = e.parseContent(),
+                content_length = (content.match(/\n/g)||[]).length + content.length;
+            
+            return content_length;
+        },
+        displayContentLength = function(e) {
+            var content_length = getContentLength(e);
+            $('#desc-counter').html('Chars: ' + content_length);
+        };
+
     $(document).ready(function() {
         $("#lf_description").markdown({
             autofocus: true,
@@ -8,12 +20,13 @@
             hiddenButtons: ["cmdPreview"],
             language: "en",
             footer: '<small id="desc-counter" class="text-muted">Chars: 0</small>',
+            onFocus: function(e) {
+                displayContentLength(e);
+            },
             onChange: function(e){
-                var content = e.parseContent(),
-                    content_length = (content.match(/\n/g)||[]).length + content.length;
-                
-                $('#desc-counter').html('Chars: ' + content_length);
+                displayContentLength(e);
             }
         });
     });
+
 })(jQuery.noConflict(true));
