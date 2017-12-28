6cc9e45d72b06408849cae9d7d3adc92c629fc26
immanuelfodor <immanuelfactor+github@gmail.com>
2017-12-28 13:39:59 +0100

    removing console log from plugin js

--- markdown_toolbar/includes/markdown_toolbar.js
+++ markdown_toolbar/includes/markdown_toolbar.js
@@ -1,6 +1,5 @@
 (function ($) {
     $(document).ready(function() {
-        console.log($.fn.jquery); // TODO: remove
         $("#lf_description").markdown({iconlibrary:"fa"});
     });
 })(jQuery.noConflict(true));
